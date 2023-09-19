967541ab641379fa021eb7f4b00ff42f1a06f288
Gurmaher <120144519+Gurmaher@users.noreply.github.com>
2023-09-19 15:21:56 -0600

    init commit

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo()
+{
+    return(
+        <div>
+            <p>Name:Gurmaher</p>
+            <p>Course section: CPRG 306 A</p>
+            <Link href="https://github.com/">https://github.com/</Link>
+        </div>
+    )
+}
