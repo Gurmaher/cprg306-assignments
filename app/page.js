d0fae74de35267f5ce34e0894f40419e723f363f
Gurmaher <120144519+Gurmaher@users.noreply.github.com>
2023-11-20 22:00:33 -0700

    week 10
    inititation

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ export default function Page()
       <p><Link href='/week6'>week6</Link></p>
       <p><Link href='/week7'>week7</Link></p>
       <p><Link href='/week8'>week8</Link></p>
+      <p><Link href='/week10'>week10</Link></p>
       </div>      
   )
 }
